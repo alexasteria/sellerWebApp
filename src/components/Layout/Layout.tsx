b8cf44061eab6894ebb89f017fdaecc1681e54ce
alexasteria <replay.faq@gmail.com>
2026-03-30 17:07:56 +0300

    feat(webapp): нижняя панель навигации включена на всех экранах

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,15 @@
 import React, { FC } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import BottomNav from './BottomNav';
 import styles from './Layout.module.css';
 
 const Layout: FC<{}> = () => {
-  const location = useLocation();
-  
-  // We might not want BottomNav on checkout/delivery page
-  const showBottomNav = location.pathname !== '/delivery';
-
   return (
     <div className={styles.layout}>
       <main className={styles.main}>
         <Outlet />
       </main>
-      {showBottomNav && <BottomNav />}
+      <BottomNav />
     </div>
   );
 };
